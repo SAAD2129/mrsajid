b34b800763dc61e4a7a5c671abe765b114369dc5
mrsaad2129@gmail.com <mrsaad2129@gmail.com>
2022-05-29 15:10:11 +0500

    Fixed Bugs

--- main.js
+++ main.js
@@ -57,25 +57,32 @@ window.onscroll = () => {
 		navigation.style.bottom = "-100%";
 	}
 };
-// Dark Mode Settings
-if (dt > 12) {
-	document.body.classList.toggle("DarkMode");
-	menuBtn.classList.toggle("inverted");
-}
-// For Width more than 400
-modeBtn.addEventListener("click", () => {
-	document.body.classList.toggle("DarkMode");
-	menuBtn.classList.toggle("inverted");
-	if (localStorage.getItem("theme") == "light") {
-		localStorage.setItem("theme", "dark");
-	} else {
-		localStorage.setItem("theme", "light");
-	}
-});
+
 // For Width 400 & less than 400
+
+BarmenuBtn.onclick = () => {
+	// navigation.classList.toggle('mob')
+	if (navigation.style.bottom == "5.5%") {
+		BarmenuBtn.src = "./icons/menu-4-fill.svg";
+		navigation.style.bottom = "-100%";
+		navigation.style.width = "101vw";
+		navItems.forEach((navItem) => {
+			navItem.style.display = "none";
+		});
+	} else {
+		navigation.style.bottom = "5.5%";
+		navigation.style.width = "101vw";
+		BarmenuBtn.src = "./icons/close-fill.svg";
+		navItems.forEach((navItem) => {
+			navItem.style.display = "block";
+		});
+	}
+};
+// Dark Mode Settings
+
 barmode.addEventListener("click", () => {
 	document.body.classList.toggle("DarkMode");
-	BarmenuBtn.classList.add("inverted");
+	menuBtn.classList.toggle("inverted");
 
 	if (localStorage.getItem("theme") == "light") {
 		localStorage.setItem("theme", "dark");
@@ -85,35 +92,33 @@ barmode.addEventListener("click", () => {
 });
 
 if (localStorage.getItem("theme") == "light") {
-	document.body.classList.remove("DarkMode");
-	BarmenuBtn.classList.remove("inverted");
-} else if (localStorage.getItem("theme") == "dark") {
 	document.body.classList.add("DarkMode");
 	BarmenuBtn.classList.add("inverted");
+	menuBtn.classList.add("inverted");
+} else if (localStorage.getItem("theme") == "dark") {
+	document.body.classList.remove("DarkMode");
+	BarmenuBtn.classList.remove("inverted");
+	menuBtn.classList.remove("inverted");
 } else {
 	localStorage.setItem("theme", "light");
+	BarmenuBtn.classList.remove("inverted");
+	menuBtn.classList.remove("inverted");
 }
-
-// Dark Mode Settings End Here;
-BarmenuBtn.onclick = () => {
-	// navigation.classList.toggle('mob')
-	if (navigation.style.bottom == "7.5%") {
-		BarmenuBtn.src = "./icons/menu-4-fill.svg";
-		navigation.style.bottom = "-100%";
-		navigation.style.width = "101vw";
-		navItems.forEach((navItem) => {
-			navItem.style.display = "none";
-		});
+// For Width more than 400
+modeBtn.addEventListener("click", () => {
+	document.body.classList.toggle("DarkMode");
+	// menuBtn.classList.toggle("inverted");
+	// BarmenuBtn.classList.toggle("inverted");
+	if (localStorage.getItem("theme") == "light") {
+		localStorage.setItem("theme", "dark");
+		menuBtn.classList.remove("inverted");
 	} else {
-		navigation.style.bottom = "7.5%";
-		navigation.style.width = "101vw";
-		BarmenuBtn.src = "./icons/close-fill.svg";
-		navItems.forEach((navItem) => {
-			navItem.style.display = "block";
-		});
+		localStorage.setItem("theme", "light");
+		menuBtn.classList.add("inverted");
 	}
-};
-
+});
+// Dark Mode Settings End Here;
+// For Form inputs
 NameMain.addEventListener("change", (e) => {
 	n = e.target.value;
 	if (e.target.value != "") {
@@ -136,7 +141,9 @@ sendBtn.addEventListener("click", () => {
 	}
 	NameContainer.innerText = n;
 });
-
+setTimeout(() => {
+	modal.style.display = "none";
+}, 2000);
 // Contact Form Data To Gmail
 // & it's Reseting
 sendBtn.onclick = () => {
